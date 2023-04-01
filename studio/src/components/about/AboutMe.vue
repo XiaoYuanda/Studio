4aaf135980a3cb0b1706441c6cb920bc92fb2057
xiaoyuanda <xiaoyuanda@aliyun.com>
2023-04-01 17:26:49 +0800

    编写个人介绍页的组件 AvatorInfo

--- studio/src/components/about/AboutMe.vue
+++ studio/src/components/about/AboutMe.vue
@@ -1,11 +1,22 @@
 <template>
-  <h1>AboutMe</h1>
+  <div>
+    <AvatorInof></AvatorInof>
+    <DescribeMe></DescribeMe>
+  </div>
 </template>
 
 <script>
+import AvatorInof from './AvatorInof.vue';
+import DescribeMe from './DescribeMe.vue'
 export default {
-  name: 'AboutMe'
+    name: "AboutMe",
+    components: { AvatorInof, DescribeMe }
 }
 </script>
-<style>
+<style scoped>
+div {
+  width:800px;
+  height: 2000px;
+  margin: 0 auto;
+}
 </style>
---
+++ studio/src/components/about/AvatorInof.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="avator">
+    <img src="../../assets/avator.jpg" alt="">
+    <ul>
+      
+      <li>
+        这是一段简介，一段描述自己的文字
+      </li>
+      <li>
+        <p>XiaoYuanda</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvatorInof'
+}
+</script>
+<style scoped>
+#avator {
+  display: flex;
+  overflow: hidden;
+  /* background-color: #eee; */
+  padding: 20px;
+  width: 800px;
+  height: 100px;
+  min-width: 800px;
+  justify-content: left;
+}
+img {
+  height: 100px;
+  width: 100px;
+  border-radius: 20px;
+}
+ul {
+ margin: 0; 
+ padding: 10px 25px;
+ display: flex;
+ flex-direction: column-reverse;
+ text-align: start;
+}
+
+ul li p {
+  font-size:xx-large;
+  font-weight: 800;
+  margin: 0;
+  padding: 0;
+}
+</style>
